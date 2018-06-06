ed6a6df86a12e45fc2795a709d6e07e7e5c6253a
John D. Aynedjian <johndavid5@yahoo.com>
2018-06-05 22:17:42 -0400

    installing tools for android...see install.bat...

--- install.bat
+++ install.bat
@@ -1,1 +1,13 @@
 npm install -g create-react-native-app
+
+https://facebook.github.io/react-native/docs/getting-started.html
+
+npm install -g react-native-cli
+C:\Users\John.diana\AppData\Roaming\npm\react-native -> C:\Users\John.diana\App
+ata\Roaming\npm\node_modules\react-native-cli\index.js
++ react-native-cli@2.0.1
+updated 1 package in 4.717s
+
+Install Android Studio -- and Android SDK 6.0 "marshmallow"
+https://developer.android.com/studio/
+(It's "only" 758 megabytes...not even 1 gigabyte...!) 
