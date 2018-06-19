0275acb1aab88ab168fe917da7c19be74b823a5a
John D. Aynedjian <johndavid5@yahoo.com>
2018-06-19 02:09:09 -0400

    Add Forecast component to WeatherProject...

--- WeatherProject/App.js
+++ WeatherProject/App.js
@@ -1,16 +1,20 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import React, { Component } from 'react';
+
 import {
   Platform,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
+
+import Forecast from "./Forecast";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -20,12 +24,44 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
 export default class App extends Component<Props> {
+
+  constructor(props){
+    super(props);
+    this.state = { zip: "", forecast: null };
+    this.version = "0.9.9";
+  }
+
+  _handleTextChange = event => {
+     let sWho = "WeatherProject/App.js::_handleTextChange";
+     console.log( `${sWho}(): event.nativeEvent.text = ${event.nativeEvent.text}...` );
+     this.setState({ zip: event.nativeEvent.text } );
+  };
+
   render() {
+
+    let content = null;
+
+    if( this.state.forecast !== null ){
+        content = (
+            <Forecast main={this.state.forecast.main}
+            description={this.state.forecast.description}
+            temp={this.state.forecast.temp}
+            />
+        );
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to The Weather Project...a ReactNative app...
+          Welcome to The Weather Project...version {this.version}
+        </Text>
+        {content}
+        <TextInput style={styles.input}
+                   onSubmitEditing={this._handleTextChange} />
+        <Text style={styles.welcome}>
+        {this.state.zip?"You inputted zip code = "+this.state.zip:"You did not input a zip code yet"}
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
@@ -36,7 +72,8 @@ export default class App extends Component<Props> {
       </View>
     );
   }
-}
+
+}/* export default class App extends Component<Props> */
 
 const styles = StyleSheet.create({
   container: {
@@ -44,6 +81,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    height: 40
   },
   welcome: {
     fontSize: 20,
